8c7f75ac843f71c68c574fbccc01a756242af018
sam atman <atmanistan@gmail.com>
2014-11-20 11:09:57 -0800

    destructuring let

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@
 
 struct VM {
     x: int,
-    y: int
+    y: int,
+    zed: Cell,
 
 }
 
+struct Cell (u32);
 
 fn main() {
-    let mut vm = VM { x: 5, y: 7 };
-    println!("VM State: x: {} , y: {}", vm.x, vm.y);
+    let mut vm = VM { x: 5, y: 7, zed: Cell(23) };
+    vm.x = 14;
+    let Cell(zed) = vm.zed;
+    println!("VM State: x: {} , y: {}, zed: {} ", vm.x, vm.y, zed  );
 
 }
