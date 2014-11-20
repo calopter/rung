6ab9afb73e396f62ce5da12b2ac0bef506414156
sam atman <atmanistan@gmail.com>
2014-11-20 11:00:31 -0800

    baby steps into VM structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// Rung: A Rust Ngaro VM
+
+
+struct VM {
+    x: int,
+    y: int
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let vm = VM { x: 5, y: 7 };
+    println!("VM State: x: {} , y: {}", vm.x, vm.y);
+
 }
