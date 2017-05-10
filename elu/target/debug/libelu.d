4bca3891687d97f82cd59752a46c0ff7b5a99a09
Aidan <calopter@gmail.com>
2017-05-10 19:19:38 +0200

    add .gitignore

--- elu/target/debug/libelu.d
+++
@@ -1,1 +0,0 @@
-/home/aidan/Documents/owncloud/rust/rung/elu/target/debug/libelu.rlib: /home/aidan/Documents/owncloud/rust/rung/elu/src/lib.rs
--- rung_vm/target/debug/librung_vm.d
+++
@@ -1,1 +0,0 @@
-/home/aidan/Documents/owncloud/rust/rung/rung_vm/target/debug/librung_vm.rlib: /home/aidan/Documents/owncloud/rust/rung/rung_vm/src/rung_vm.rs
