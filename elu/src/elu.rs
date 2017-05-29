48a5a3cd1ac38ee14f67ef25014a2b3572f75b74
cynlic <montogmeryh@kenyon.edu>
2017-05-29 14:54:07 -0500

    This isn't perfect by any means but I changed some things such that pass1() will be easier to reason about. Also the "i " directive seems to be working. Also tests!

--- elu/src/elu.rs
+++ elu/src/elu.rs
@@ -15,6 +15,7 @@ pub fn save(target: &[i32; IMAGE_SIZE], path: &str) {
     for i in target.iter() {
         file.write_i32::<LittleEndian>(*i);
     }
+}
 
 fn opcode_for(s: &str) -> i32 {
     match s {
@@ -28,22 +29,44 @@ fn opcode_for(s: &str) -> i32 {
     }
 }
 
-//fn pack_opcode(instructions: &Chars) {}
 
-fn pass1(path: &str, target: &[i32; IMAGE_SIZE], labels: &HashMap<&str, u8>) {
+fn i_pass <'a> (line: String, target: &mut Vec<i32>) {
+    let mut v: Vec<&str> = vec!{};
+    
+    for x in 0..line.len() {
+        if x%2 == 0 {
+            let (first, rest) = line.split_at(x);
+            let (token, unused) = rest.split_at(2);
+            println!("Value to be printed:\"{}\" ", token);
+            v.push(token);
+        }
+    }
+    for x in 1..v.len()-1{
+        println!("Inst: {} \t Op: {}", v[x], opcode_for(v[x]));
+        target.push(opcode_for(v[x]));
+    }
+}
+fn pass1(path: &str, target: &mut Vec<i32>, labels: &HashMap<&str, u8>) {
     let file = File::open(path).unwrap();
     let mut reader = BufReader::new(file);
     let mut line_number: usize = 1;
 
     for line in reader.lines() {
         line_number += 1;
-        match line.unwrap().chars().next().unwrap() {
-            'i' => (), //chars, drop 2, split into 2s, get opcodes, append to target
-            'r' => (), //append 4 dummy u8s to target
-            'd' => (), //chars, drop 2, parse as u8(how many chars?), append to target
-            'c' => (), // chars, drop 2, atoi, append target
-            's' => (), // chars, drop 2, map atoi, append all to target, 0 terminate?
-            ':' => (), // chars, drop 2, rest to string, insert into hash with line_number
+        let this_line = line.unwrap().clone();
+        let line_data = this_line.clone();
+        let mut first = this_line.split_whitespace();
+
+        match first.next() {
+            Some("i") => i_pass(line_data, target), //chars, drop 2, split into 2s, get opcodes, append to target
+            Some("r") => target.push(-1), //append 4 dummy u8s to target
+            Some("d") => target.push(first.next()
+                                     .unwrap()
+                                     .parse()
+                                     .unwrap_or(0)), //chars, drop 2, parse as u8(how many chars?), append to target
+            Some("c") => (), // chars, drop 2, atoi, append target
+            Some("s") => (), // chars, drop 2, map atoi, append all to target, 0 terminate?
+            Some(":") => (), // chars, drop 2, rest to string, insert into hash with line_number
              _  => (),
         }
     }
@@ -59,4 +82,24 @@ const IMAGE_SIZE: usize = 1024;
 fn main() {
     let mut target: [i32; IMAGE_SIZE] = [0; IMAGE_SIZE];
     let mut labels: HashMap<&str, u8> = HashMap::new();
+    let mut target2: Vec<i32> = vec![];
+    let instruction: String = "i: liju....".to_string();
+
+    i_pass(instruction, &mut target2);
+    for x in 0..target2.len()-1{
+        println!("{}", target2[0]);
+    }
+}
+
+
+#[test]
+fn it_works(){
+    let mut target2: Vec<i32> = vec![];
+    let instruction: String = "i liju....".to_string();
+    println!("This is the instruction to be printed \"{}\"  ", instruction);
+    i_pass(instruction, &mut target2);
+    for x in 0..target2.len()-1{
+         println!("This is the memory layout of target: {}", target2[x]);
+     }
+    assert_eq!(target2[0], 1);
 }
